31d905790ac25e99d88ab5c233883e25196f9c3f
bhargavasai227 <112853876+bhargavasai227@users.noreply.github.com>
2024-05-15 10:36:32 +0530

    gradient

--- app/Nav.js
+++ app/Nav.js
@@ -1,7 +1,7 @@
 "use client"
 export default function Nav(){
 return(
-<div className="h-[5vh] w-full px-[3rem] flex items-center " >
+<div className="h-[5vh] w-full px-[3rem] flex items-center bg-gradient-to-b from-gray-800 to-black " >
     <p>aiLaw</p>
 </div>
 );
